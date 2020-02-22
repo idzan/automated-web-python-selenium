3d2cb868632c6fd22660e1441eb8557d995d0bb2
Marko Idzan <42409619+idzan@users.noreply.github.com>
2020-02-23 00:26:08 +0100

    Update automate.py
    Yes, loads my channel :wink: Can be tweaked for other channel hah

--- automate.py
+++ automate.py
@@ -2,3 +2,9 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 driver.get('https://youtube.com')
+
+searchbox = driver.find_element_by_xpath('//*[@id="search"]')
+searchbox.send_keys('Marko Idzan')
+
+searchbutton = driver.find_element_by_xpath('//*[@id="search-icon-legacy"]')
+searchbutton.click()
